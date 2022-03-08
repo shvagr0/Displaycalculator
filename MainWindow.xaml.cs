e497472f1a2d692a39d544084316f85d1b20faf1
Shvagr0 <vladislav.rozvadovski@kpk-lp.com.ua>
2022-03-08 13:44:14 +0200

    Add first Maket

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,116 +28,116 @@ namespace MonitorCalculator
 
             foreach (UIElement el in MainGrid.Children)
             {
-                if (el is Button)
-                {
-                    ((Button)el).Click += Button_Click;
-                }
-                else if (el is CheckBox)
-                {
-                    ((CheckBox)el).Checked += CheckBox_Click;
-                }
+                //if (el is Button)
+                //{
+                //    ((Button)el).Click += Button_Click;
+                //}
+                //else if (el is CheckBox)
+                //{
+                //    ((CheckBox)el).Checked += CheckBox_Click;
+                //}
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (CheckBoxMode.IsChecked == true)
-            {
-                bool setSide = (bool)RadioButtW.IsChecked;
-                double sideL;
-                try
-                {
-                    if (setSide)
-                    {
-                        sideL = double.Parse(wResult.Text);
-                    }
-                    else
-                    {
-                        sideL = double.Parse(hResult.Text);
-                    }
-                    var monitor = new Display(new Corralation(12, 4), sideL, Display.Side.Width);
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (CheckBoxMode.IsChecked == true)
+        //    {
+        //        bool setSide = (bool)RadioButtW.IsChecked;
+        //        double sideL;
+        //        try
+        //        {
+        //            if (setSide)
+        //            {
+        //                sideL = double.Parse(wResult.Text);
+        //            }
+        //            else
+        //            {
+        //                sideL = double.Parse(hResult.Text);
+        //            }
+        //            var monitor = new Display(new Corralation(12, 4), sideL, Display.Side.Width);
 
-                    diagonal.Text = string.Format("{0:0.0}", monitor.diagonal);
+        //            diagonal.Text = string.Format("{0:0.0}", monitor.diagonal);
 
-                    if (setSide)
-                    {
-                        hResult.Text = string.Format("{0:0.000}", monitor.height);
-                    }
-                    else
-                    {
-                        wResult.Text = string.Format("{0:0.000}", monitor.width);
-                    }
-                }
-                catch { }
+        //            if (setSide)
+        //            {
+        //                hResult.Text = string.Format("{0:0.000}", monitor.height);
+        //            }
+        //            else
+        //            {
+        //                wResult.Text = string.Format("{0:0.000}", monitor.width);
+        //            }
+        //        }
+        //        catch { }
 
-            }
-            else
-            {
-                try
-                {
-                    Corralation corralation = new Corralation(12, 4);
-                    var monitor = new Display(double.Parse(diagonal.Text), new Corralation(12, 2));
+        //    }
+        //    else
+        //    {
+        //        try
+        //        {
+        //            Corralation corralation = new Corralation(12, 4);
+        //            var monitor = new Display(double.Parse(diagonal.Text), new Corralation(12, 2));
 
-                    hResult.Text = string.Format("{0:0.000}", monitor.height);
-                    wResult.Text = string.Format("{0:0.000}", monitor.width);
+        //            hResult.Text = string.Format("{0:0.000}", monitor.height);
+        //            wResult.Text = string.Format("{0:0.000}", monitor.width);
 
-                }
-                catch { }
-            }
-        }
+        //        }
+        //        catch { }
+        //    }
+        //}
 
-        private void CheckBox_Click(object sender, RoutedEventArgs e)
-        {
-            Color colorResultBox = Color.FromRgb(230, 230, 130);
-            Color defaultColorBox = Color.FromRgb(255, 255, 255);
-            if (CheckBoxMode.IsChecked == false)
-            {
-                #region IsEnabled bools
-                wResult.Style = (Style)Resources["CloseBox"];
-                hResult.Style = (Style)Resources["CloseBox"];
+        //private void CheckBox_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Color colorResultBox = Color.FromRgb(230, 230, 130);
+        //    Color defaultColorBox = Color.FromRgb(255, 255, 255);
+        //    if (CheckBoxMode.IsChecked == false)
+        //    {
+        //        #region IsEnabled bools
+        //        wResult.Style = (Style)Resources["CloseBox"];
+        //        hResult.Style = (Style)Resources["CloseBox"];
 
-                diagonal.Style = (Style)Resources["OpenBox"];
-                correlation.Style = (Style)Resources["OpenBox"];
+        //        diagonal.Style = (Style)Resources["OpenBox"];
+        //        correlation.Style = (Style)Resources["OpenBox"];
 
-                RadioButtW.Visibility = Visibility.Hidden;
-                if (RadioButtH != null)
-                    RadioButtH.Visibility = Visibility.Hidden;
-                #endregion
-            }
-            else
-            {
-                #region IsEnabled bools
-                wResult.Style = (Style)Resources["OpenBox"];
-                hResult.Style = (Style)Resources["OpenBox"];
+        //        RadioButtW.Visibility = Visibility.Hidden;
+        //        if (RadioButtH != null)
+        //            RadioButtH.Visibility = Visibility.Hidden;
+        //        #endregion
+        //    }
+        //    else
+        //    {
+        //        #region IsEnabled bools
+        //        wResult.Style = (Style)Resources["OpenBox"];
+        //        hResult.Style = (Style)Resources["OpenBox"];
 
-                diagonal.Style = (Style)Resources["CloseBox"];
-                correlation.Style = (Style)Resources["OpenBox"];
+        //        diagonal.Style = (Style)Resources["CloseBox"];
+        //        correlation.Style = (Style)Resources["OpenBox"];
 
-                if (RadioButtW.IsChecked == true)
-                {
-                    hResult.Style = (Style)Resources["CloseBox"];
-                }
-                else
-                {
-                    wResult.Style = (Style)Resources["CloseBox"];
-                }
+        //        if (RadioButtW.IsChecked == true)
+        //        {
+        //            hResult.Style = (Style)Resources["CloseBox"];
+        //        }
+        //        else
+        //        {
+        //            wResult.Style = (Style)Resources["CloseBox"];
+        //        }
 
-                RadioButtH.Visibility = Visibility.Visible;
-                RadioButtW.Visibility = Visibility.Visible;
-                #endregion
-            }
-        }
+        //        RadioButtH.Visibility = Visibility.Visible;
+        //        RadioButtW.Visibility = Visibility.Visible;
+        //        #endregion
+        //    }
+        //}
 
-        private void OnlyNumbers(object sender, TextCompositionEventArgs e)
-        {
-            Regex regex = new Regex(@"[\d,]+");
-            e.Handled = regex.IsMatch(e.Text);
-        }
+        //private void OnlyNumbers(object sender, TextCompositionEventArgs e)
+        //{
+        //    Regex regex = new Regex(@"[\d,]+");
+        //    e.Handled = regex.IsMatch(e.Text);
+        //}
 
-        private void correlation_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            Regex regex = new Regex(@"^[0-9]+:*\d*");
-            e.Handled = regex.IsMatch(e.Text);
-        }
+        //private void correlation_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        //{
+        //    Regex regex = new Regex(@"^[0-9]+:*\d*");
+        //    e.Handled = regex.IsMatch(e.Text);
+        //}
     }
 }
