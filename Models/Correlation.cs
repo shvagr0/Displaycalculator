66dae85ffbe381ac02f271a3485a5096f6bdfa4a
shvagr0 <93011174+shvagr0@users.noreply.github.com>
2024-05-08 00:12:35 +0200

    Update Correlation.cs

--- Models/Correlation.cs
+++ Models/Correlation.cs
@@ -16,69 +16,15 @@ namespace DisplayCalculator
             {
                 this.Height = 1;
                 this.Width = 1;
-                return;
             }
-
-            this.Height = Height;
-            this.Width = Width;
-
-            List<uint> BList = GetDivisors(Width);
-            List<uint> SList = GetDivisors(Height);
-            SortLists(ref SList, ref BList);
-            SList.Sort();
-            BList.Sort();
-
-            uint cof = 1;
-            for (int s = 0; s < SList.Count; s++)
+            else
             {
-                for (int b = 0; b < BList.Count; b++)
-                {
-                    if (BList[b] == SList[s])
-                    {
-                        cof *= BList[b];
-                        BList.RemoveAt(b);
-                        break;
-                    }
-                }
+                this.Height = Height / gcd(Height, Width);
+                this.Width = Width / gcd(Height, Width);
             }
-            this.Width /= cof;
-            this.Height /= cof;
-
         }
-
-        private List<uint> GetDivisors(uint num)
-        {
-            List<uint> List = new List<uint>();
-            uint number = num;
-            while (true)
-            {
-                bool IsNatural = false;
-                for (uint i = 2; i <= 5; i++)
-                {
-                    if (i == 4) continue;
-                    if (number % i == 0)
-                    {
-                        number /= i;
-                        List.Add(i);
-                        break;
-                    }
-                    IsNatural = true;
-                }
-                if (IsNatural)
-                {
-                    List.Add(number);
-                    break;
-                }
-            }
-            return List;
-        }
-        private void SortLists(ref List<uint> smallList, ref List<uint> bigList)
-        {
-            if (smallList.Count == bigList.Count || smallList.Count < bigList.Count) return;
-            List<uint> tmp = smallList;
-            smallList = bigList;
-            bigList = tmp;
-        }
+        
+        private int gcd(int a, int b) => b == 0 ? a : gcd(b, a % b);
 
         public override string ToString()
         {
